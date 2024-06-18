8a9e00ade9ee8aa619f53090454dc60ed36d99d0
jivern <jivern@student.21-school.ru>
2024-06-18 16:38:30 +0300

    solution L14 junit5

--- Lesson_6/src/test/java/jivern/FactorialAppTest.java
+++ Lesson_6/src/test/java/jivern/FactorialAppTest.java
@@ -1,38 +1,96 @@
 package jivern;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
-/**
- * Unit test for simple App.
- */
-public class FactorialAppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public FactorialAppTest(String testName )
-    {
-        super( testName );
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FactorialAppTest {
+    @Test
+    @Order(1)
+    public void testFactorialAppZero() {
+        String input = "0\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        FactorialApp.main(new String[]{});
+
+        String expectedOutput = "Введите число: Факториал числа 0 равен 1\n";
+        assertEquals(expectedOutput, outContent.toString());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( FactorialAppTest.class );
+    @Test
+    @Order(2)
+    public void testFactorialAppOne() {
+        String input = "1\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        FactorialApp.main(new String[]{});
+
+        String expectedOutput = "Введите число: Факториал числа 1 равен 1\n";
+        assertEquals(expectedOutput, outContent.toString());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    @Order(3)
+    public void testFactorialAppThree() {
+        String input = "3\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        FactorialApp.main(new String[]{});
+
+        String expectedOutput = "Введите число: Факториал числа 3 равен 6\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    @Order(4)
+    public void testFactorialAppFive() {
+        String input = "5\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        FactorialApp.main(new String[]{});
+
+        String expectedOutput = "Введите число: Факториал числа 5 равен 120\n";
+        assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    @Order(5)
+    public void testFactorialAppSeven() {
+        String input = "7\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        FactorialApp.main(new String[]{});
+
+        String expectedOutput = "Введите число: Факториал числа 7 равен 5040\n";
+        assertEquals(expectedOutput, outContent.toString());
     }
 }
