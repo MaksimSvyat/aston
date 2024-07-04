21e4ec921fa1c09ad767ba8fe8f04f4d2cf016c4
jivern <jivern@student.21-school.ru>
2024-07-04 15:39:47 +0300

    solution L17

--- Lesson_8/src/test/java/BasePage.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.WebDriver;
-
-public abstract class BasePage {
-    protected static WebDriver driver;
-
-    public static void setDriver(WebDriver webDriver) {
-        driver = webDriver;
-    }
-}
